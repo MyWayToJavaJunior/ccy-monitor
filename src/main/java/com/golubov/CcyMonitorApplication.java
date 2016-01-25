5585f4dda0c64f4b39078879da80afae5f70be3d
sgolubov <sergeygolubov@gmail.com>
2016-01-25 16:59:37 +0100

    some refactoring

--- src/main/java/com/golubov/CcyMonitorApplication.java
+++ src/main/java/com/golubov/CcyMonitorApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -20,7 +19,6 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
-@Configuration
 @ComponentScan
 @SpringBootApplication
 @EnableJpaRepositories
